5fb4096bf2f17d86ddcb99158142815a38b08b1e
vanshika858 <vanshika.rbu@gmail.com>
2023-12-24 10:19:26 +0530

    edit leave schema and create combined schema containing leave,salary,attendance

--- Backend/leave.js
+++ Backend/leave.js
@@ -1,33 +1,57 @@
 const mongoose = require("mongoose");
 
-const leaveSchema = new mongoose.Schema({
-  Employee: {
+const combined = new mongoose.Schema({
+  employeeId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Employee",
     required: true,
   },
-  startDate: {
-    type: Date,
-    required: true,
-  },
-  endDate: {
-    type: Date,
-    required: true,
-  },
-  reason: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["Pending", "Approved", "Rejected"],
-    default: "Pending",
-  },
-  approvedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-  },
+  attendance:[
+    {
+      checkInTime:{
+        type:Date,
+        required:true
+      },
+      checkOutTime:{
+        type:Date
+      },
+      date:{
+        type:Date,
+        required:true
+      },
+    },
+  ],
+  leave:[
+    {
+      leaveStartDate:{
+        type:Date,
+        required:true
+      },
+      leaveEndDate:{
+        type:Date,
+        required:true
+      },
+      leaveType:String,
+      status:String,
+    },
+  ],
+  salary:[
+    {
+      salaryAmount:{
+        type:Number,
+        required:true
+      },
+      salaryDate:{
+        type:Date,
+        required:true
+      },
+      deduction:{
+        type:Number,
+        default:0
+      },
+    },
+  ],
 });
 
-const Leave = mongoose.model("leaves", leaveSchema);
-module.exports = Leave;
+const combinedModel = mongoose.model("Combined", combined);
+module.exports = combinedModel;
